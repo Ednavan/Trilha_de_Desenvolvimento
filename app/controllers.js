eb240ac29c7e8b30b41a4f26232322b6d416ee79
Ednavan <ednavanfisico@gmail.com>
2021-08-18 20:03:12 -0300

    configuração inicial de produtos

--- app/controllers.js
+++ app/controllers.js
@@ -1,10 +1,11 @@
 angular.module("meuModulo").controller("indexController",function($scope){
     $scope.titulo = "FILTRO";
-    $scope.produtos = [
+    $scope.arrayProdutos = [
 
-
-       {nome1: "Cabo de Rede Patch Cord CAT5E RJ45 Azul Exbom CBX-N5C10 1 METRO"},
-        {nome2: "Celular Xiaomi Note 8 64GB Rom 4GB Ram Dual Versión Global Space Black"}
+        {produto: "Notebook",descricao: "DellBrasil", valor:"R$ 3.197,00 "},
+        {produto: "Cabo de Enthernet",descricao: "Amazon", valor:"R$ 220,00"},
+        {produto: "Mouse",descricao: "logitech", valor:"R$ 140,00"},
+        {produto: "Celular Xiaomi",descricao: "Amazon", valor:"R$ 987,00"},
        
     ];
 });
