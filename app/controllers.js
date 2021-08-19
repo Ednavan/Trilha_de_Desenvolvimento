a0aba55476fbc3c09caa5bfd5dd2935f612bf9a8
Ednavan <ednavanfisico@gmail.com>
2021-08-18 21:42:20 -0300

    layout dos produtos horizontal

--- app/controllers.js
+++ app/controllers.js
@@ -2,10 +2,14 @@ angular.module("meuModulo").controller("indexController",function($scope){
     $scope.titulo = "FILTRO";
     $scope.arrayProdutos = [
 
-        {produto: "Notebook",descricao: "DellBrasil", valor:"R$ 3.197,00 "},
+        {produto: "Notebook",descricao: "DellBrasil", valor:"R$ 3.197,00" }, /*com o img, quero armazenar imagem*/
         {produto: "Cabo de Enthernet",descricao: "Amazon", valor:"R$ 220,00"},
         {produto: "Mouse",descricao: "logitech", valor:"R$ 140,00"},
-        {produto: "Celular Xiaomi",descricao: "Amazon", valor:"R$ 987,00"},
+        {produto: "Celular Xiaomi",descricao: "Amazon", valor:"R$ 987,00",},
        
     ];
+
+
+   
 });
+
