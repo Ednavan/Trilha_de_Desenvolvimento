f18e91bc4a0b7b2109f7ace5a99d7942ce470652
Ednavan <ednavanfisico@gmail.com>
2021-08-23 10:55:24 -0300

    modal adicionaProduto configurando

--- app/controllers.js
+++ app/controllers.js
@@ -2,14 +2,18 @@ angular.module("meuModulo").controller("indexController", function ($scope) {
     $scope.titulo = "FILTRO";
     $scope.arrayProdutos = [
 
-        { produto: "Notebook", descricao: "DellBrasil", valor: "R$ 3.197,00" }, /*com o img, quero armazenar imagem*/
+        { produto: "Notebook", descricao: "DellBrasil", valor: "R$ 3.197,00",  }, /*com o img, quero armazenar imagem*/
         { produto: "Cabo de Enthernet", descricao: "Amazon", valor: "R$ 220,00" },
         { produto: "Mouse", descricao: "logitech", valor: "R$ 140,00" },
         { produto: "Celular Xiaomi", descricao: "Amazon", valor: "R$ 987,00" },
 
     ];
-
-
+   
+    $scope.adicionaProduto = function(){
+        $scope.arrayProdutos.push({produto: $scope.lista.produto});
+        $scope.lista.produto = '';
+        
+    };
 
 });
 
@@ -23,3 +27,4 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.modal');
     var instances = M.Modal.init(modal2);
   });
+  
