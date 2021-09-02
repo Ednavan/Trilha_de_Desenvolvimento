e5211658b05181e7044229e33c0f1b0e3c50c3cc
Ednavan <ednavanfisico@gmail.com>
2021-09-02 13:25:10 -0300

    modificações em: mo input valor, adicionando validações de mascara

--- js/controller.js
+++ js/controller.js
@@ -2,3 +2,21 @@ angular.module("meuModulo").controller("indexController", function ($scope) {
     $scope.titulo = "FILTRO";
 
 });
+
+
+$(document).ready(function(){
+    $("#inputValorProduto").mask("R$ 0,00")
+})
+
+
+
+/*
+function mascara_valor(){
+    var valor = document.getElementById('inputValorProduto')
+    if(valor.value.length === 1 || valor.value.length == 2 ){
+       valor.value =  valor.value + ","
+    }
+    
+}
+
+*/
