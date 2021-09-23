d0bd30f59851de3a1abd4e779f02518634c4690d
Ednavan <ednavanfisico@gmail.com>
2021-09-23 15:25:25 -0300

    Caso a quantidade e/ou valor do produto seja 0, o produto deverá ser ocultado da listagem na tela inicial.

--- js/controller.js
+++ js/controller.js
@@ -1,120 +1,137 @@
 angular.module("meuModulo").controller("indexController", function ($scope, $http) {
-    
-    $scope.data={};
-           $scope.submit=function(){
-            if( $("#inputProduto").val() !="" & $('#inputDescricao').val() != "" & $('#valor').val() != "" &  $('#inputQtd').val() != "" ){
-        console.log('clicked sumit');
-        $http({
-            url:'http://localhost:3000/arrayProduto',
-            method: 'POST',
-            data: $scope.data,
-       
-        }).then(function (){
-            
-            $http.get('http://localhost:3000/arrayProduto').
-            then(function(res){
+
+    $scope.data = {};
+    $scope.submit = function () {
+
+        if ($("#inputProduto").val() != "" & $('#inputDescricao').val() != "" & $('#valor').val() != "" & $('#inputQtd').val() != "" ) {
+           
+           
+            console.log('clicked sumit');
+            $http({
+                url: 'http://localhost:3000/arrayProduto',
+                method: 'POST',
+                data: $scope.data,
+
+            }).then(function () {
+
+                $('#modalAdiciona').modal('toggle')
+
+            }).catch(function () {
+
+                $('#modalErro').modal('toggle')
+
+            }
+            )
+        
+        } else {
+            alert("Não é possível cadastrar objetos sem conter todos os dados!")
+       } 
+    }
+
+    $http.get('http://localhost:3000/arrayProduto').
+        then(function (res) {
             (console.log(res))
             $scope.arrayProduto = res.data;
 
-            $('#modalAdiciona').modal('toggle')
-           
-    })
-                        
-        }).catch(function(){
-           
-            $('#modalErro').modal('toggle')
-           
-           }
-       )
-    }else{
-        alert("Não é possível cadastrar objetos sem conter todos os dados!")
-    }}
+        })
 
-    
-    $http.get('http://localhost:3000/arrayProduto').
-    then(function(res){
-    (console.log(res))
-    $scope.arrayProduto = res.data;
-   
-    })
 
-    $scope.editar = function(a){
-        
+
+
+
+    $scope.editar = function (a) {
+
         $scope.myVar = a;
-        console.log(a)
-    }   
+
+
     }
-    
-   
-);
+
+    $scope.enviardados = function () {
+        $http({
+            url: 'http://localhost:3000/arrayProduto/' + $scope.myVar.id, //seleciona o dados atraves do id
+            data: $scope.myVar, //envia os dados atraves do seu conjunto    
+            method: "patch", //é mais importante é responsável pela ação do método
+
+        }).then(function (tratamento) {
+            console.log("SUCCESS");
+            console.log(tratamento)
+            // $scope.arrayProduto = data.items
+        }).catch(function () {
+            alert("Eroo")
+        })
+
+    }
+
+
+});
 
 
 /*bloqueia modal quando clica fora da página */
-$(document).ready(function(){
-$('#modalAdiciona').modal({backdrop: 'static', keyboard: false}) 
+$(document).ready(function () {
+    $('#modalAdiciona').modal({ backdrop: 'static', keyboard: false })
 
 });
 
 /*bloqueia modal quando clica fora da página */
-$(document).ready(function(){
-$("#exampleModal").modal({backdrop:'static', keyboard:false})
+$(document).ready(function () {
+    $("#exampleModal").modal({ backdrop: 'static', keyboard: false })
 
 });
 
 
 /*bloqueia modal quando clica fora da página */
-$(document).ready(function(){
-    $('#modalErro').modal({backdrop: 'static', keyboard: false}) 
-    
+$(document).ready(function () {
+    $('#modalErro').modal({ backdrop: 'static', keyboard: false })
+
 });
 
-    
+
 
 /*configuração  de campo de validacao caso clique em (sim)*/
-function limpa(){
-    if(document.getElementById('inputProduto').value!=""){
-        document.getElementById('inputProduto').value="",
-        document.getElementById('inputDescricao').value="",
-        document.getElementById('valor').value="",
-        document.getElementById('inputQtd').value="",
-        $("#modalAdiciona").modal('hide');
+function limpa() {
+    if (document.getElementById('inputProduto').value != "") {
+        document.getElementById('inputProduto').value = "",
+            document.getElementById('inputDescricao').value = "",
+            document.getElementById('valor').value = "",
+            document.getElementById('inputQtd').value = "",
+            $("#modalAdiciona").modal('hide');
     }
 }
 
 
-function preserv(){
+function preserv() {
     $("#modalErro").modal('hide');
 }
 
-function preservCancelar(){
+function preservCancelar() {
     $("#exampleModal").modal('hide');
 }
 
- $(function(){
+$(function () {
     $("#mascara").maskMoney({
-       prefix: 'R$ ',
-       allowNegative: true,
-       thousands: '.',
-       decimal: ','
+        prefix: 'R$ ',
+        allowNegative: true,
+        thousands: '.',
+        decimal: ','
     });
- });
+});
 
 
-function formatarMoeda(){
+function formatarMoeda() {
     var elemento = document.getElementById('valor');
     var valor = elemento.value;
 
     valor = valor + '';
     valor = parseInt(valor.replace(/[\D]+/g, ''));
     valor = valor + '';
-    valor = valor.replace( /([0-9]{2})$/g, ",$1");
+    valor = valor.replace(/([0-9]{2})$/g, ",$1");
 
-    if(valor.length > 6){
+    if (valor.length > 6) {
         valor = valor.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2");
 
     }
     elemento.value = valor;
-    if(valor == 'NaN') elemento.value = '';
+    if (valor == 'NaN') elemento.value = '';
 }
 
 
